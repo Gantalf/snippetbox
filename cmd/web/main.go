92b99278f1a65614bff982d27a6b13aa631abc97
Luciano <pereiraluciano169@gmail.com>
2024-09-09 15:19:03 -0300

    add open db configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,9 +19,19 @@ func main() {
 	addr := flag.String("addr", ":4000", "HHTP network address")
 	flag.Parse() //en caso que se escriba algo en la linea de commando lo lee y asgina a addr
 
+	// Define a new command-line flag for the MySQL DSN string.
+	dsn := flag.String("dsn", "web:webUser1.@tcp(127.0.0.1:3306)/snippetbox?parseTime=true", "MySQL data source name")
+
 	//parametros: el destino donde escribira el log, un prefijo que va a tener el msj y flags para indicar que informacion extra va a incluir
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -34,7 +47,18 @@ func main() {
 
 	//log.Printf("Starting server on %s", *addr)
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	//log.Fatal(err)
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
